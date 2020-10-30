f1cb8da354f5ef8839a721d7b0b5b023d032e5b1
Srivats Venkataraman <srivats.venkataraman@gmail.com>
2020-10-30 09:24:14 -0700

    Refactored code

---
+++ bookMng/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class MainMenu(models.Model):
+    item = models.CharField(max_length=200, unique=True)
+    link = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.item

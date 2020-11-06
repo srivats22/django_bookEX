0a318c8114e7b798bc9e6278c7fc50c5c91f696a
j-as-o-n <j.istejada29@gmail.com>
2020-11-06 02:23:25 -0800

    bookMng/models.py ->     Created the Book class. bookMng/views.py ->     Instead of using base.html for the index, updated it to use bookMng/home.html. We also store the MainMenu contents in 'item_list'.

--- bookMng/models.py
+++ bookMng/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class MainMenu(models.Model):
@@ -7,3 +8,16 @@ class MainMenu(models.Model):
 
     def __str__(self):
         return self.item
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=200)
+    web = models.URLField(max_length=300)
+    price = models.DecimalField(decimal_places=2, max_digits=6)
+    publish_date = models.DateField(auto_now=True)
+    picture = models.FileField(upload_to='bookEx/static/uploads')
+    pic_path = models.CharField(max_length=300, editable=False)
+    user_name = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.id)
--- bookMng/views.py
+++ bookMng/views.py
@@ -3,12 +3,15 @@ from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from .models import MainMenu
 
 
 # Create your views here.
 def index(request):
     # return HttpResponse("Hello World")
-    return render(request, 'base.html')
+    return render(request, 'bookMng/home.html',
+        { 'item_list': MainMenu.objects.all() }
+    )
 
 
 def home(request):

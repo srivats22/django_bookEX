c8ef1bd9a6ff37d86a13b243b27c22e98274e3aa
wilsonfx0306 <53382319+wilsonfx0306@users.noreply.github.com>
2020-11-11 23:26:28 -0800

    Addition of Contact Form
    Added contact form to be included in the book_detail.html page. Changes were made to urls.py, views.py, and settings.py.

--- bookMng/urls.py
+++ bookMng/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('displaybooks', views.displaybooks, name='displaybooks')
+    # urlpattern for contact form.
+    path('book_detail', views.contact, name="contact"),
 ]
